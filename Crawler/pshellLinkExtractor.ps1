61e581716d546906bb1108e70a0dcbb5e374b635
AJK55 <40635902+AJK55@users.noreply.github.com>
2020-11-11 09:28:50 -0500

    Add files via upload

---
+++ Crawler/pshellLinkExtractor.ps1
@@ -0,0 +1,7 @@
+﻿Clear-Host
+$WebResponse = Invoke-WebRequest "http://www.coach.com"
+#$WebResponse
+$WebResponse.Links | Select-Object href | Where-Object {$_.href -like “http*”}
+#$WebResponse.Links | Select href | Where-Object {$_.href -like “http*”} | Export-Csv -Path C:\Users\aklinga\Desktop\Crawler\pshellCrawl.csv -NoTypeInformation -Append
+#$WebResponse.Links | Select href | Where-Object {$_.href -like “http*”} | Out-File C:\Users\aklinga\Desktop\Crawler\pshellCrawl.txt -Append
+$WebResponse.Links | Select-Object href | Where-Object {$_.href -like “http*”} | Out-File $PSScriptRoot\pshellCrawl.txt -Append
